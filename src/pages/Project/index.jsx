36ebc81efc1ab681004d0419e44815ecf9409dda
Quentin Dumon <quentindumon352a@gmail.com>
2024-03-11 11:13:36 +0100

    Correction de la redirection vers la page projet au milieu de celle-ci

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,11 +1,16 @@
 import "./Project.scss"
 import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 import projects from "../../datas/projets.json"
 import ProjectSection from "../../components/ProjectSection"
 function Project() {
+    const { hash } = useLocation()
     useEffect(() => {
         document.title = "Projets - Quentin Dumon"
-    }, [])
+        if (hash === "") {
+            window.scrollTo(0, 0)
+        }
+    }, [hash])
     return (
         <main className="main__container">
             <div className="main__container-projects">
